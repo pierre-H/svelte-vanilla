98a6f4ec6d7e80de8bb5ddd173507a3cf297df2a
PIRNAY <pirnay@outlook.com>
2024-06-04 10:19:13 +0200

    Add Badge

---
+++ src/lib/Badge/Badge.svelte
@@ -0,0 +1,19 @@
+<script lang="ts" context="module">
+	import type { Snippet } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	export type Props = HTMLAttributes<HTMLSpanElement> & {
+		negative?: boolean;
+		'aria-label': string;
+	};
+</script>
+
+<script lang="ts">
+	let { children, negative, ...restProps }: Props = $props();
+</script>
+
+<span class:p-badge={!negative} class:p-badge--negative={negative} {...restProps}>
+	{#if children}
+		{@render children()}
+	{/if}
+</span>
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,5 @@
 export * from './Accordion/index.js';
 export * from './CodeSnippet/index.js';
+export { default as Badge } from './Badge/Badge.svelte';
 export { default as Button } from './Button/Button.svelte';
 export * from './Form/index.js';
